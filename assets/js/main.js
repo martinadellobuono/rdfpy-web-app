3c2a5cb7a7d8bcab1dcd8004d90a81bc0162abdf
martinadellobuono <34475729+martinadellobuono@users.noreply.github.com>
2023-06-11 12:10:14 +0200

    backend

---
+++ assets/js/main.js
@@ -0,0 +1,151 @@
+document.addEventListener("DOMContentLoaded", () => {
+  checkDeviceW();
+  selectTripleComp();
+  rdfData();
+});
+
+/* check the width of device */
+let checkDeviceW = () => {
+  /* remove flex to allow scroll on small devices */
+  let removeFlex = () => {
+    if (window.innerWidth < 992) {
+      document.querySelector("main").classList.remove("d-flex", "flex-nowrap");
+      document.querySelector(".section-closed").classList.remove("text-rotate");
+    } else {
+      document.querySelector("main").classList.add("d-flex", "flex-nowrap");
+      document.querySelector(".section-closed").classList.add("text-rotate");
+    };
+  };
+  /* on document ready */
+  removeFlex();
+  /* on resize */
+  window.addEventListener("resize", () => {
+    removeFlex();
+  });
+};
+
+/* select triple subject and object */
+let selectTripleComp = () => {
+  /* subject */
+  var biblStr;
+
+  /* object / array of bibl */
+  var biblArr = [];
+
+  /* set subject/predicate/object attribute to submit button */
+  var submit = document.querySelectorAll(".submit-data");
+
+  /* subject / object */
+  var txts = document.querySelectorAll(".select-txt");
+  txts.forEach((txt) => {
+    txt.addEventListener(("click"), (e) => {
+      /* data */
+      var tripleRole = e.target.getAttribute("data-triple");
+      var author = e.target.getAttribute("data-author");
+      var title = e.target.getAttribute("data-title");
+      var pubdate = e.target.getAttribute("data-dataPub");
+      biblStr = "<b>" + author + "</b>" + "<span>, </span>" + "<i>" + title + "</i>" + "<span>, </span>" + "<span>" + pubdate + "</span>";
+
+      /* set the data in the subject/object of the triple */
+      var tripleComps = document.querySelectorAll("." + tripleRole);
+      tripleComps.forEach((comp) => {
+
+        /* if object add/remove bibl from array because of checkbox */
+        if (comp.classList.contains("object")) {
+          comp.classList.add("btn-triple");
+
+          /* add checked texts to array */
+          if (e.target.checked) {
+            if (!biblArr.includes(biblStr)) {
+              biblArr.push(biblStr);
+            };
+          } else {
+            if (biblArr.includes(biblStr)) {
+              const index = biblArr.indexOf(biblStr);
+              if (index > -1) {
+                biblArr.splice(index, 1);
+              };
+            };
+          };
+
+          /* print the bibl str */
+          if (comp.innerHTML !== "") {
+            comp.innerHTML = "";
+
+            if (biblArr.length > 1) {
+              biblArr.forEach((bibl) => {
+                comp.innerHTML += "<span class='btn-triple-divider'>" + bibl + "</span>";
+              });
+            } else {
+              biblArr.forEach((bibl) => {
+                comp.innerHTML += bibl;
+              });
+            };
+          } else {
+            comp.innerHTML = "object";
+          };
+
+          /* submit button / object */
+          var obj = biblArr.join("___");
+
+          submit.forEach((btn) => {
+            btn.setAttribute("data-object", obj);
+          });
+
+        } else {
+          /* print the bibl str */
+          comp.classList.add("btn-triple");
+          comp.innerHTML = biblStr;
+
+          /* submit button / object */
+          submit.forEach((btn) => {
+            btn.setAttribute("data-subject", biblStr);
+          });
+        };
+      });
+
+      /* expand abbreviation */
+      expandAbbreviations();
+    });
+  });
+
+  /* predicate */
+  var selectPredicate = document.querySelectorAll(".select-predicate");
+  selectPredicate.forEach((select) => {
+    select.addEventListener("change", (e) => {
+      var options = e.target;
+      var predicate = options.value;
+      /* submit button / predicate */
+      submit.forEach((btn) => {
+        btn.setAttribute("data-predicate", predicate);
+      });
+    });
+  });
+};
+
+/* expand abbreviations */
+let expandAbbreviations = () => {
+  var abbreviations = document.querySelectorAll(".triple-comp");
+  abbreviations.forEach((abbr) => {
+    abbr.addEventListener(("click"), () => {
+      abbr.classList.toggle("btn-triple");
+    });
+  });
+};
+
+/* save data for rdf generation */
+let rdfData = () => {
+  /* click on submit buttons */
+  var submits = document.querySelectorAll(".submit-data");
+  submits.forEach((submit) => {
+    submit.addEventListener("click", () => {
+      /* strip html tags */
+
+      /* objects */
+      var objs = submit.getAttribute("data-object").split("___");
+      objs.forEach((obj) => {
+        console.log(obj.replace(/(<([^>]+)>)/gi, ""));
+      });
+    });
+  });
+};
---
+++ views.py
@@ -0,0 +1,56 @@
+from flask import Blueprint, render_template
+from SPARQLWrapper import SPARQLWrapper, JSON
+import uuid
+
+""" blueprint """
+views = Blueprint(__name__, "views")
+
+""" endpoint """
+endpoint = "https://query.wikidata.org/sparql"
+sparql = SPARQLWrapper(endpoint)
+
+""" query """
+query = """
+    PREFIX bd: <http://www.bigdata.com/rdf#>
+    PREFIX p: <http://www.wikidata.org/prop/>
+    PREFIX psn: <http://www.wikidata.org/prop/statement/value-normalized/>
+    PREFIX wd: <http://www.wikidata.org/entity/>
+    PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+    PREFIX wikibase: <http://wikiba.se/ontology#>
+    SELECT ?island ?islandLabel ?islandImage
+    WITH {
+        SELECT DISTINCT *
+        WHERE {
+            ?island (wdt:P31/wdt:P279*) wd:Q23442.
+            OPTIONAL { ?island wdt:P18 ?islandImage. }
+            ?island p:P2046/psn:P2046/wikibase:quantityAmount ?islandArea.
+        }
+        ORDER BY DESC(?islandArea)
+        LIMIT 100
+    } AS %i
+    WHERE {
+        INCLUDE %i
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" . }
+    }
+    ORDER BY DESC(?islandArea) 
+"""
+
+""" home """
+@views.route("/")
+def index():
+    return render_template("index.html")
+
+""" data """
+@views.route("/data")
+def altro():
+    """ convert data into JSON """
+    sparql = SPARQLWrapper(endpoint)
+    sparql.setTimeout(55)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    queryResults =  sparql.query().convert()
+
+    for result in queryResults["results"]["bindings"]:
+        print(result)
+
+    return render_template("data.html")
